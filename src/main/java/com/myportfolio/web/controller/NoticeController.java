12c0464f7c4af0f6ecb0ef0933a06961dd11a4d4
imazato-lee <klmzzz711@gmail.com>
2023-06-02 20:00:22 +0900

    NoticeList.jsp

---
+++ src/main/java/com/myportfolio/web/controller/NoticeController.java
@@ -0,0 +1,20 @@
+package com.myportfolio.web.controller;
+
+import com.myportfolio.web.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice/*")
+public class NoticeController {
+
+    @Autowired
+    NoticeService noticeService;
+
+    @GetMapping("/list")
+    public String list(){
+        return "noticeList";
+    }
+}
